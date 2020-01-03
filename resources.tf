2163c47be1b6f8897b0ddade12c126d942bd1a9f
anisanis611 <anis.dhouieb@gmail.com>
2020-01-03 19:43:33 +0100

    formatting the files with the fmt command

--- resources.tf
+++ resources.tf
@@ -1,44 +1,44 @@
 resource "aws_vpc" "vpc-example-1" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    tags = {
-      Name  = "Terraform-aws-vpc-example"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "Terraform-aws-vpc-example"
+  }
 }
 
 resource "aws_subnet" "subnet1" {
 
-    cidr_block= "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 3, 1)}"
-    vpc_id = "${aws_vpc.vpc-example-1.id}"
-    availability_zone = "eu-west-3a"
+  cidr_block        = "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.vpc-example-1.id}"
+  availability_zone = "eu-west-3a"
 }
 
 
 
 resource "aws_subnet" "subnet2" {
 
-    cidr_block= "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 2, 2)}"
-    vpc_id = "${aws_vpc.vpc-example-1.id}"
-    availability_zone = "eu-west-3a"
+  cidr_block        = "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.vpc-example-1.id}"
+  availability_zone = "eu-west-3a"
 }
 
 
 resource "aws_security_group" "subnet-security" {
-    vpc_id = "${aws_vpc.vpc-example-1.id}"
-    ingress  {
+  vpc_id = "${aws_vpc.vpc-example-1.id}"
+  ingress {
 
-        cidr_blocks = [
+    cidr_blocks = [
 
-            "${aws_vpc.vpc-example-1.cidr_block}"
+      "${aws_vpc.vpc-example-1.cidr_block}"
 
-        ]
+    ]
 
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
 
 
-    } 
+  }
 
 }
