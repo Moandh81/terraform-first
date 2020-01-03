00e9b69eb89ce3c94e35f2f0ef7c2938fe98e7c9
anisanis611 <anis.dhouieb@gmail.com>
2020-01-03 19:27:45 +0100

    add subnet and security group

--- resources.tf
+++ resources.tf
@@ -6,3 +6,39 @@ resource "aws_vpc" "vpc-example-1" {
       Name  = "Terraform-aws-vpc-example"
     }
 }
+
+resource "aws_subnet" "subnet1" {
+
+    cidr_block= "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 3, 1)}"
+    vpc_id = "${aws_vpc.vpc-example-1.id}"
+    availability_zone = "eu-west-3a"
+}
+
+
+
+resource "aws_subnet" "subnet2" {
+
+    cidr_block= "${cidrsubnet(aws_vpc.vpc-example-1.cidr_block, 2, 2)}"
+    vpc_id = "${aws_vpc.vpc-example-1.id}"
+    availability_zone = "eu-west-3a"
+}
+
+
+resource "aws_security_group" "subnet-security" {
+    vpc_id = "${aws_vpc.vpc-example-1.id}"
+    ingress  {
+
+        cidr_blocks = [
+
+            "${aws_vpc.vpc-example-1.cidr_block}"
+
+        ]
+
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+
+
+    } 
+
+}
